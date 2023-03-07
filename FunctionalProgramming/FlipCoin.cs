321ec969bddaacc6c8b09f1b8fed83f6d40193d4
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-07 19:29:11 +0530

    [Ganesh] Add:Flip coin is added

---
+++ FunctionalProgramming/FlipCoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class FlipCoin
+    {
+        public void flip(int numOfFlips)
+        {
+            int head = 0, tail = 0, count = 0;
+            while(count< numOfFlips)
+            {
+                Random random = new Random();
+                double coin=random.NextDouble();
+                if (coin < 0.5)
+                    tail++;
+                else
+                    head++;
+                count++;
+            }
+            double headPercentage = (head * 100) / numOfFlips;
+            double tailPercentage = (tail * 100) / numOfFlips;
+            Console.WriteLine("Head percentage----> " + headPercentage + "\nTail percentage----> " + tailPercentage);
+        }
+    }
+}
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+
 namespace FunctionalProgramming
 {
     public class Program
@@ -6,6 +8,27 @@ namespace FunctionalProgramming
         static void Main(String[] args)
         {
             Console.WriteLine("Welcome to functional programming");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Coin Flip\n2.Exit\nEnter your option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin coin = new FlipCoin();
+                        Console.WriteLine("Enter num of flips of coin");
+                        int num= Convert.ToInt32(Console.ReadLine());
+                        coin.flip(num);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+                
+            }
         }
+        
     }
+
 }
