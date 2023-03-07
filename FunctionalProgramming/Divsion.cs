ce4d1f08489632298e958fce6cd4bd1b158fb13c
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-07 20:46:34 +0530

    [Ganesh] Add: quotient and rem updated

---
+++ FunctionalProgramming/Divsion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class Divsion
+    {
+        public void operation(int num1,int num2)
+        {
+            int quotient = num1 / num2;
+            int remainder = num1 % num2;
+            Console.WriteLine("The quotient is " + quotient + "\n the remainder is " + remainder);
+        }
+    }
+}
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -11,7 +11,7 @@ namespace FunctionalProgramming
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Coin Flip\n2.leap year\n3.Power of 2\n4.Harmoic progression\n5.Prime factors\n6.Exit\nEnter your option to execute");
+                Console.WriteLine("1.Coin Flip\n2.leap year\n3.Power of 2\n4.Harmoic progression\n5.Prime factors\n6. Quotient and remainder\n7.Exit\nEnter your option to execute");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -46,6 +46,14 @@ namespace FunctionalProgramming
                         pf.factors(n);
                         break;
                     case 6:
+                        Divsion divsion = new Divsion();
+                        Console.WriteLine("Enter the first number");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        divsion.operation(num1, num2);
+                        break;
+                    case 7:
                         flag = false;
                         break;
                 }
